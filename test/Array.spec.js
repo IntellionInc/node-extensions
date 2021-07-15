e53ca0acb2f44f97534c79fcb0621cacc0e58ba8
mmmikolay <tuncer.ilk@gmail.com>
2021-07-15 13:36:08 +0300

    stricter behavior definition

--- test/Array.spec.js
+++ test/Array.spec.js
@@ -1,11 +1,6 @@
 import "../src/Array.js";
 import { expect } from "./matchalatte.js";
 
-const arrayEqual = (a1, a2) =>
-  [a1, a2].every((a) => Array.isArray(a)) &&
-  a1.length === a2.length &&
-  a1.every((item, i) => a2[i] === item);
-
 describe("Array", () => {
   let array1 = ["c", "d", 1, 3];
 
@@ -22,51 +17,55 @@ describe("Array", () => {
   describe("append", () => {
     let arr = ["c", "d", 1, 3];
     let items = ["$", "a"];
-    let result = ["c", "d", 1, 3, "$", "a"];
+    let output = ["c", "d", 1, 3, "$", "a"];
     it("should push the arguments to the array and return the array ", () => {
-      arr.append(...items);
-      arrayEqual(arr, result);
+      const result = arr.append(...items);
+      expect(result).to.eq(arr);
+      expect(result).to.eql(output);
     });
   });
   describe("removeLast", () => {
     let arr = ["c", "d", 1, 3];
-    let result = ["c", "d", 1];
+    let output = ["c", "d", 1];
     it("should remove the last element from the array and return the array", () => {
-      arr.removeLast();
-      expect(arrayEqual(arr, result)).to.eq(true);
+      let result = arr.removeLast();
+      expect(result).to.eq(arr);
+      expect(result).to.eql(output);
     });
   });
-});
-describe("looselyEquals", () => {
-  context("when arrays include only primitives", () => {
-    let arr = ["c", "d", 1, 3];
-    context("when loosely equals", () => {
-      let result = ["c", "d", 1, 3];
-      it("should return true", () => {
-        expect(arrayEqual(arr, result)).to.eq(true);
+  describe("looselyEquals", () => {
+    context("when arrays include only primitives", () => {
+      let arr = ["c", "d", 1, 3];
+      context("when loosely equals", () => {
+        let comparison = ["c", "d", 1, 3];
+        it("should return true", () => {
+          expect(arr.looselyEquals(comparison)).to.eq(true);
+          expect(arr).to.eql(comparison);
+        });
+      });
+      context("when not loosely equals", () => {
+        let comparison = ["c", "d", 3, 1];
+        it("should return false", () => {
+          expect(arr.looselyEquals(comparison)).to.eq(false);
+          expect(arr).to.not.eql(comparison);
+        });
       });
     });
-    context("when not loosely equals", () => {
-      let result = ["c", "d", 3, 1];
-      it("should return false", () => {
-        expect(arrayEqual(arr, result)).to.eq(false);
+    context("when arrays include non-primitive values", () => {
+      let obj1 = { some: "obj" };
+      let arr = ["c", "d", 1, obj1];
+      context("when all values and references are the same", () => {
+        let comparison = ["c", "d", 1, obj1];
+        it("should return true", () => {
+          expect(arr.looselyEquals(comparison)).to.eq(true);
+        });
       });
-    });
-  });
-  context("when arrays include non-primitive values", () => {
-    let obj1 = { some: "obj" };
-    let arr = ["c", "d", 1, obj1];
-    context("when all values and references are the same", () => {
-      let result = ["c", "d", 1, obj1];
-      it("should return true", () => {
-        expect(arrayEqual(arr, result)).to.eq(true);
-      });
-    });
-    context("when some references are different", () => {
-      let obj2 = { some: "obj" };
-      let result = ["c", "d", 1, obj2];
-      it("should return false", () => {
-        expect(arrayEqual(arr, result)).to.eq(false);
+      context("when some references are different", () => {
+        let obj2 = { some: "obj" };
+        let comparison = ["c", "d", 1, obj2];
+        it("should return false", () => {
+          expect(arr.looselyEquals(comparison)).to.eq(false);
+        });
       });
     });
   });
