a24e489b65e80e1f82e7ab0ce9dc9970826ebddb
mmmikolay <tuncer.ilk@gmail.com>
2021-07-19 13:27:05 +0300

    integer time extensions converter functions

--- src/Number.ts
+++ src/Number.ts
@@ -5,6 +5,13 @@ interface Number {
   days: number;
   months: number;
   years: number;
+
+  inYears(): number;
+  inMonths(): number;
+  inDays(): number;
+  inHours(): number;
+  inMinutes(): number;
+  inSeconds(): number;
 }
 
 Object.defineProperties(Number.prototype, {
@@ -41,6 +48,43 @@ Object.defineProperties(Number.prototype, {
     get: function () {
       const a = 365;
       return this * a.days;
+    },
+  },
+  
+  inYears: {
+    value: function () {
+      const a = 1;
+      return this / a.years;
+    },
+  },
+  inMonths: {
+    value: function () {
+      const a = 1;
+      return this / a.months;
+    },
+  },
+  inDays: {
+    value: function () {
+      const a = 1;
+      return this / a.days;
+    },
+  },
+  inHours: {
+    value: function () {
+      const a = 1;
+      return this / a.hours;
+    },
+  },
+  inMinutes: {
+    value: function () {
+      const a = 1;
+      return this / a.minutes;
+    },
+  },
+  inSeconds: {
+    value: function () {
+      const a = 1;
+      return this / a.seconds;
     },
   },
 });
--- test/Number.spec.ts
+++ test/Number.spec.ts
@@ -43,4 +43,47 @@ describe("Number", () => {
       expect(input.years).toBe(output);
     });
   });
+
+  describe("inYears", () => {
+    let input = 31536000000;
+    let output = 1;
+    it("should convert the value from milliseconds to years", () => {
+      expect(input.inYears()).toBe(output);
+    });
+  });
+  describe("inMonths", () => {
+    let input = 2592000000;
+    let output = 1;
+    it("should convert the value from milliseconds to months", () => {
+      expect(input.inMonths()).toBe(output);
+    });
+  });
+  describe("inDays", () => {
+    let input = 31536000000;
+    let output = 365;
+    it("should convert the value from milliseconds to days", () => {
+      expect(input.inDays()).toBe(output);
+    });
+  });
+  describe("inHours", () => {
+    let input = 31536000000;
+    let output = 365 * 24;
+    it("should convert the value from milliseconds to hours", () => {
+      expect(input.inHours()).toBe(output);
+    });
+  });
+  describe("inMinutes", () => {
+    let input = 31536000000;
+    let output = 365 * 24 * 60;
+    it("should convert the value from milliseconds to minutes", () => {
+      expect(input.inMinutes()).toBe(output);
+    });
+  });
+  describe("inSeconds", () => {
+    let input = 31536000000;
+    let output = 365 * 24 * 60 * 60;
+    it("should convert the value from milliseconds to seconds", () => {
+      expect(input.inSeconds()).toBe(output);
+    });
+  });
 });
