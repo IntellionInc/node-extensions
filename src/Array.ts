be7b7bcf60a7c26130ba8cae0fd59114a4431643
mmmikolay <tuncer.ilk@gmail.com>
2021-07-19 20:18:19 +0300

    array documentation strings

--- src/Array.ts
+++ src/Array.ts
@@ -1,10 +1,31 @@
 declare global {
   interface Array<T> {
+    /**
+     * Gets the first element of the array.
+     */
     first: T;
+    /**
+     * Gets the last element of the array.
+     */
     last: T;
+    /**
+     * Appends new elements to the enf of an array, and returns the array.
+     * @param args New elements to add to the array.
+     */
     append(...args: any[]): any[];
+    /**
+     * Removes the last element from an array and returns the array.
+     */
     removeLast(): T[];
+    /**
+     * Checks if the contents of two arrays are equal up to a depth of 1.
+     * @param arr Array to be compared with the base array.
+     */
     looselyEquals(arr: any[]): boolean;
+    /**
+     * Deconstructs a specific property fro man array of objects, and returns a new array consisting of these properties.
+     * @param key Name of the property to be plucked.
+     */
     pluck(key: string): any[];
   }
 }
