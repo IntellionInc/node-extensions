18722b1e8ae05bd54ce05e732408886b41f3d9e7
mmmikolay <tuncer.ilk@gmail.com>
2021-08-24 15:58:59 +0300

    reverted jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,12 @@
 export default {
-	moduleNameMapper: {
-		"^src/(.*)$": "<rootDir>/src/$1"
-	},
-	roots: ["<rootDir>/test"],
-	testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
-	transform: {
-		"^.+\\.(ts|tsx)$": "ts-jest"
-	}
-};
+  "roots": [
+    "<rootDir>/test"
+  ],
+  "testMatch": [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+}
