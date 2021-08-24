c77423249d7e01d9cd22604a6677df48e4778c55
mmmikolay <tuncer.ilk@gmail.com>
2021-08-24 15:21:15 +0300

    jest config for absolute paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,10 @@
 export default {
-  "roots": [
-    "<rootDir>/test"
-  ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-}
+	moduleNameMapper: {
+		"^src/(.*)$": "<rootDir>/src/$1"
+	},
+	roots: ["<rootDir>/test"],
+	testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
+	transform: {
+		"^.+\\.(ts|tsx)$": "ts-jest"
+	}
+};
